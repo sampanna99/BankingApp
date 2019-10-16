02420bbff218e50c441b817f6f45dfc6b8d935ef
Sampanna Pokhrel <sampanna99@gmail.com>
2019-10-15 22:13:19 -0400

    testt

---
+++ MVC_ATMDEM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_ATMDEM.Startup))]
+namespace MVC_ATMDEM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
